8334fa48cb83157df15ab282d8f8068f5104d820
sarthaksarangi <sarthakrohan5@gmail.com>
2023-05-22 23:45:55 +0530

    stuck: cant install codemirror

--- codepen/src/components/Home.jsx
+++ codepen/src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import Code from'./Code';
 const Home = () => {
   return (
-    <div>
+    <>
      <Header/>
-    </div>
+     <Code/>
+    
+    </>
   )
 }
 
